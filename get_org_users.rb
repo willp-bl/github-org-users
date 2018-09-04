49fb732ff99a56afa767c755f07d9f4d2bf9500c
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-09-04 12:07:59 +0100

    remove unused info

--- get_org_users.rb
+++ get_org_users.rb
@@ -21,37 +21,34 @@ client = Octokit::Client.new(:access_token => "#{ENV[github_token_env_var]}")
 # so we don't have to manually handle pagination in the responses from the api
 client.auto_paginate = true
 
-def print_user(login, name, email, state, role, inviter, membership)
+def print_user(login, name, email, role, membership)
     info = { username: login,
                   name: name,
                   email: email,
-                  state: state,
                   role: role,
-                  inviter: inviter,
                   membership: membership }
-    puts "#{login}, #{name}, #{state}, #{role}, #{inviter}, #{membership}, "
+    puts "#{login}, #{name}, #{role}, #{membership}, "
     info
 end
 
-puts "login, name, state, role, inviter, membership, "
+puts "login, name, role, membership, "
 
 members = client.organization_members(github_org).map { |member|
     membership = client.organization_membership(github_org, { user: member.login })
     user_info = client.user member.login
-    print_user(member.login, user_info.name, user_info.email, membership.state, membership.role, 'null', 'member')
+    print_user(member.login, user_info.name, user_info.email, membership.role, 'member')
 }
 
 invitations = client.organization_invitations(github_org).map { |member|
     membership = client.organization_membership(github_org, { user: member.login })
     user_info = client.user member.login
-    pending_message = 'invitation_pending'
-    print_user(member.login, user_info.name, user_info.email, pending_message, membership.role, membership.inviter.login, pending_message)
+    print_user(member.login, user_info.name, user_info.email, membership.role, 'invitation_pending')
 }
 
 collaborators = client.outside_collaborators(github_org).map { |member|
     user_info = client.user member.login
     os_message = 'outside_collaborator'
-    print_user(member.login, user_info.name, user_info.email, os_message, os_message, 'null', os_message)
+    print_user(member.login, user_info.name, user_info.email, os_message, os_message)
 }
 
 all_users = members + invitations + collaborators
